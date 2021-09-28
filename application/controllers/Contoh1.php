4dafff3b50d8417bb741e0380b9f35c99670a2c9
arfankur <12200015@bsi.ac.id>
2021-09-29 00:23:44 +0700

    final commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,8 @@
+<?php class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Arfan Kurnianto Saya tingga di daerah Bogor olah raga yang saya sukai adalah Mukul Kacang";
+    }
+}
